a5797e299992f5b60b3ca39822acfdb9cf0bf609
aravindmatelabs <aravind@matelabs.in>
2020-03-16 13:32:01 +0530

    selected and uploaded image

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ dropRegion.addEventListener('dragover', preventDefault, false)
 dropRegion.addEventListener('drop', preventDefault, false)
 dropRegion.addEventListener('drop', handleDrop, false)
 
+let imagesToBeDisplayed = []
+
 function preventDefault(e) {
   e.preventDefault()
 }
@@ -16,19 +18,50 @@ function handleDrop(e) {
 }
 
 function handleFiles(files) {
-  files = [...files]
-  console.log(files)
-  files.forEach(file => {
-    let reader = new FileReader() // used to read the file from the users remote system
-    reader.readAsDataURL(file)
-    console.log(reader)
-    reader.onloadend = function() {
-      var img = document.createElement('img')
+  for (let i = 0; i < files.length; i++) {
+    const file = files[i]
 
-      img.src = reader.result
-      img.setAttribute('width', '200px')
-
-      document.getElementById('display').appendChild(img)
-    }
-  })
+    imagesToBeDisplayed.push(file)
+    displayFilename(file.name)
+  }
 }
+
+function displayFilename(name) {
+  const li = document.createElement('li')
+  const filename = document.createTextNode(name)
+  li.appendChild(filename)
+
+  var uploadimage = document.getElementById('listimages').appendChild(li)
+}
+
+function uploadButtonHandler() {
+  imagesToBeDisplayed.forEach(imageFile => {
+    renderImageFromFile(imageFile)
+  })
+
+  clearImageArray()
+}
+
+function clearImageArray() {
+  imagesToBeDisplayed = []
+  document.getElementById('listimages').innerHTML = ''
+}
+
+function renderImageFromFile(file) {
+  let reader = new FileReader() // used to read the file from the users remote system
+
+  reader.readAsDataURL(file)
+
+  reader.onload = function() {
+    var img = document.createElement('img')
+
+    img.src = reader.result
+    img.setAttribute('width', '200px')
+
+    document.getElementById('display').appendChild(img)
+  }
+}
+
+document
+  .querySelector('#upload_button')
+  .addEventListener('click', uploadButtonHandler)
